3a5f15e6343c2b9a3a43b3eed085ccfdc25708c0
roberto pesto <akrom1999@hotmail.com>
2016-09-08 16:00:33 -0300

    Se realizo el console.log para ver los datos que se cargan y luego se ajustaron varios errores de carga inicial en el registro de usuarios

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
 
-var app = angular.module('LoginAngular', []);
+/*var app = angular.module('LoginAngular', ['angularSpinner']);
+
+
+app.controller('controlLogin', function($scope,usSpinnerService,$rootScope) {
+
+    $scope.usuario={};
+    $scope.usuario.email = "fede@fede.com";
+    $scope.usuario.password = "federico";
+    $scope.spinneractive = false;
+
+    $scope.Login=function(){
+
+    	console.log("login a retener:");
+    	console.log($scope.usuario);
+        usSpinnerService.spin('spinner-1'); 
+        $scope.spinneractive = true;
+        if  (( $scope.usuario.email == "fede@fede.com" &&
+    		$scope.usuario.password == "federico") ||
+    		( $scope.usuario.email == "cwitt@cwitt.com" &&
+    		$scope.usuario.password == "123456" ))
+		 usSpinnerService.stop('spinner-1');	   	  
+	  }
+
+});*/
+
+var app = angular.module('LoginAngular',[]);
 
 
 app.controller('controlLogin', function($scope) {
@@ -7,6 +32,13 @@ app.controller('controlLogin', function($scope) {
     $scope.usuario={};
     $scope.usuario.email = "fede@fede.com";
     $scope.usuario.password = "federico";
+    $scope.spinneractive = false;
+
+    $scope.Login=function(){
+
+    	console.log("login a retener:");
+    	console.log($scope.usuario);
+    }
 
 });
 
@@ -15,14 +47,20 @@ app.controller('controlAlta', function($scope) {
     $scope.usuario={};
 	$scope.usuario.nombre = "Juan";
 	$scope.usuario.apellido = "Gonzalez";
-	$scope.usuario.edad = "38";
+	$scope.usuario.edad = 38;
 	$scope.usuario.sexo = "Masculino";
-	$scope.usuario.dni = "154";
+	$scope.usuario.dni = 23444444;
 	$scope.usuario.estado = "opcion1";
-	$scope.usuario.fechaNacimiento = "1978-11-07";
+	$scope.usuario.fechaNacimiento = new Date("1999-03-25");
 	$scope.usuario.email = "juan@juan.com";
 	$scope.usuario.password = "contrasena";
 	$scope.usuario.copiapassword = "contrasena";
-	$scope.usuario.lenguaje = "Javascript";
+    
+	$scope.Registrar=function(){
+
+    	console.log("usuario a crear:");
+    	console.log($scope.usuario);
+     	   	  
+	  }
 
 });
